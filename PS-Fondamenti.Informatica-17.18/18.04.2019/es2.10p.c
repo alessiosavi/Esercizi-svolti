fb54513a25d095cb50f3ea4c4ccbf6af94138e07
alessiosavi <alessiosavibtc@gmail.com>
2019-06-22 18:30:38 +0200

    Create folder structure for organizec code

---
+++ PS-Fondamenti.Informatica-17.18/18.04.2019/es2.10p.c
@@ -0,0 +1,14 @@
+/*
+ *Data una lista di numeri interi in forma collegata con i puntatori, scrivere:
+ * - Metodo che verifichi che la lista sia ordinata;
+ * - Metodo che, date due liste in input, ritorni una nuova lista avente gli
+ *   elementi della lista in input "intercalati" tra loro [A=[2,5,9] ; B=[4,1,8]
+ *   C=[2,4,5,1,9,8]
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
