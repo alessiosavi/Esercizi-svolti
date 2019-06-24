9ce0d1a363c3a6891cca75e4630a87a46fe4b778
AlessioSavi <alessio.savi-cic-it@ibm-com>
2019-06-24 20:18:01 +0200

    Implement first basic List Add example on basic assignament concept

---
+++ Concetti-base/puntatore.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_es01(int a,int b,int c){
+  printf("\nA: %d | B: %d | C: %d",a,b,c);
+}
+
+void esempio_01(){
+  int a=0;
+  int b=1;
+  int *c = &b;
+  print_es01(a,b,*c);
+
+}
+
+
+
+
+int main(){
+
+  esempio_01();
+
+  return 0;
+}
