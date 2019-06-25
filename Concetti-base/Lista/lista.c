363c995207e4e8195faa36a9db95325a748369b2
AlessioSavi <alessio.savi-cic-it@ibm-com>
2019-06-25 15:51:45 +0200

    v0.0.1

--- Concetti-base/Lista/lista.c
+++ Concetti-base/Lista/lista.c
@@ -16,20 +16,62 @@ struct node *init_node(int value) {
   return head;
 }
 
-void add_node(struct node **head, int value) {
+/* Swap two node using their memory addresses */
+void swap_node(struct node **A, struct node **B) {
+  struct node *temp = NULL;
+  temp = *A;
+  *A = *B;
+  *B = temp;
+  // free(temp); /*FIXME: This will panic!! You have to free the object that you
+  // have swap [A] after call this method */
+  return;
+}
 
+void add_node_root(struct node **head, int value) {
+
+  /* Init ROOT node */
+  if (*head == NULL) {
+    *head = init_node(value);
+    printf("\nRoot node initialized [%d]", value);
+  } else if ((*head)->next == NULL) {
+    /* No other element except root, replacing! */
+    printf("\nReplacing root node with [%d]", value);
+    *head = init_node(value);
+  } else {
+    struct node *root = init_node(value);
+    root->next = (*head)->next;
+    swap_node(&root, head);
+    free(root);
+  }
+}
+
+/*
+ * add_node is delegated to initalize a new node and add to the list.
+ * If the pointer passed in input is related to a NULL object, it will
+ *  initialize a new root node
+ * Input:
+ *  - head -> memory address of pointer to 'node object' (pointer to -> struct
+ *   node *head);
+ *  - value -> data to store;
+ * */
+
+void add_node(struct node **head, int value) {
   if (*head == NULL) {
     *head = init_node(value);
     printf("\nRoot node initialized [%d]", value);
     return;
   }
 
+  /* Copy the ELEMENT from the pointer in input */
   struct node *temp = *head;
 
+  /* Iterating element until there is no next */
   while (temp->next != NULL) {
     temp = temp->next;
   }
-  printf("\nAdding node %d", value);
+  /* At this point the node (temp->next) is NULL, so we are going to add a new
+   * node */
+  // printf("\nAdding node %d", value);
   temp->next = init_node(value);
   return;
 }
@@ -37,6 +79,7 @@ void add_node(struct node **head, int value) {
 void print_node(struct node *head) {
   if (head == NULL) {
     printf("\nprint_node | Call the init function, there is no list there :D");
+    return;
   }
   printf("\n");
   while (head != NULL) {
@@ -66,11 +109,12 @@ int remove_node(struct node **head, int value) {
   printf("\nremove_node | iterating value ...");
   while (current->next != NULL) {
     if (current->next->value == value) { /* Data found! */
+
       swap_node = current->next;
       current->next = swap_node->next;
       free(swap_node);
 
-      // current = (*head)->next; /* try with current->next*/
+      // current = (*head)->next;
       // free(*head);
       //*head = current;
 
@@ -79,7 +123,7 @@ int remove_node(struct node **head, int value) {
     }
     current = current->next;
   }
-  printf("\nData not found - last");
+  printf("\nData not found");
   return -1;
 }
 
@@ -94,5 +138,7 @@ void init_n_node(struct node **head, int n) {
   clock_t end = clock();
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   float it = (float)n / time_spent;
-  printf("init_n_node | Add %d element in %f [%f it/s]", n, time_spent, it);
+  printf("\ninit_n_node | Add %d element in %f [%f it/s]", n, time_spent, it);
 }
+
+// int main(){return 0 ;}
