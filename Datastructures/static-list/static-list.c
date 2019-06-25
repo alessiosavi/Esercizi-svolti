6a75cfd13f13f261cdb146f3ec66694e21160504
alessiosavi <alessiosavibtc@gmail.com>
2019-06-25 21:22:44 +0200

    Implement remove node

--- Datastructures/static-list/static-list.c
+++ Datastructures/static-list/static-list.c
@@ -51,11 +51,11 @@ struct list *init_list(int size, int value) {
   /* the size rappresent the number of node inserted in the list (only the root
    * for now) */
   list->size =
-      0; /* The node just add, will increase when the value will be set few
-            instruction below */
+    0; /* The node just add, will increase when the value will be set few
+          instruction below */
   list->max_size = size; /* Parameter used for allocate the space */
   list->array[list->size++] =
-      value; /* array that contains the value just inserted */
+    value; /* array that contains the value just inserted */
   return list;
 }
 
@@ -66,33 +66,73 @@ void add_node(struct list **data, int value) {
     printf("\nadd_node | INIT ROOT NODE %d", value);
     *data = init_list(MIN_LIST_LENGHT, value);
   } else if /* check if is necessary to allocate a new element */
-      ((*data)->size == (*data)->max_size) {
-    /* List have reached the maximum space! Allocate some new fresh memory */
+    ((*data)->size == (*data)->max_size) {
+      /* List have reached the maximum space! Allocate some new fresh memory */
 
-    /* Access and save the old size */
-    int old_max_size = (*data)->max_size;
-    /* Increase the size by a constant factor */
-    int new_size = old_max_size + DEFAULT_INC;
+      /* Access and save the old size */
+      int old_max_size = (*data)->max_size;
+      /* Increase the size by a constant factor */
+      int new_size = old_max_size + DEFAULT_INC;
 
-    printf("\nadd_node | Allocate a new space for store %d", value);
+      printf("\nadd_node | Allocate a new space for store %d", value);
 
-    /*Allocate a new space for the data */
-    int *new_list = malloc(new_size * sizeof(int));
-    /* Copying the data ... (destination, source, size) */
-    memcpy(new_list, (*data)->array, old_max_size * sizeof(int));
-    /* Free the data */
-    free((*data)->array);
-    /* Copy the pointer to the data*/
-    (*data)->array = new_list;
-    /* Set the new max size*/
-    (*data)->max_size = new_size;
-    /* Add the element to the new list and use the post increment to increase
-     * the size*/
-    (*data)->array[(*data)->size++] = value;
-  } else {
-    printf("\nadd_node | Insert -> %d | POS: %d", value, (*data)->size);
-    (*data)->array[(*data)->size++] = value;
+      /*Allocate a new space for the data */
+      int *new_list = malloc(new_size * sizeof(int));
+      /* Copying the data ... (destination, source, size) */
+      memcpy(new_list, (*data)->array, old_max_size * sizeof(int));
+      /* Free the data */
+      free((*data)->array);
+      /* Copy the pointer to the data*/
+      (*data)->array = new_list;
+      /* Set the new max size*/
+      (*data)->max_size = new_size;
+      /* Add the element to the new list and use the post increment to increase
+       * the size*/
+      (*data)->array[(*data)->size++] = value;
+    } else {
+      printf("\nadd_node | Insert -> %d | POS: %d", value, (*data)->size);
+      (*data)->array[(*data)->size++] = value;
+    }
+}
+
+void remove_node(struct list **data, int value) {
+
+  if (*data == NULL || (*data)->array == NULL) {
+    printf("\nremove_node | LIST EMPTY!");
+    return;
   }
+
+  int counter = 0;
+  /* Iterating the data ... */
+
+  while (counter < (*data)->max_size) {
+    /* Data found! */
+    if ((*data)->array[counter] == value) {
+      /* Delete the address copying the memory */
+
+      int new_size = (*data)->max_size+DEFAULT_INC - 1;
+      /* This will contains the new data */
+      int *new_list = malloc(new_size*sizeof(int));
+      /* Copy everything before the element */
+      memcpy(new_list,(*data)->array,(counter)*sizeof(int));
+      /* Copy everything after the element */
+      memcpy(&new_list[counter],&(*data)->array[counter+1],((*data)->max_size-(counter))*sizeof(int));
+
+      memset(&new_list[counter],-1,(new_size-counter) * sizeof(int));
+
+
+      /* Free the data */
+      free((*data)->array);
+      /* Copy the pointer to the data*/
+      (*data)->array = new_list;
+      /* Set the new max size*/
+      (*data)->max_size = new_size;
+      printf("\nremove_node | Done!");
+      return;
+    }
+    ++counter;
+  }
+  printf("\remove_node | Data [%d] not found ... ",value);
 }
 
 int main() {
@@ -102,9 +142,12 @@ int main() {
   print_nodes(list);
   int i = 1;
 
-  for (; i < 100; i++) {
+  for (; i < 10; i++) {
     add_node(&list, i);
   }
+
+  print_nodes(list);
+  remove_node(&list,9);
 
   print_nodes(list);
 }
