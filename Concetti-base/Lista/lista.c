4c9a2c053dc9da7ae6d7df8b5dfc1baf81cc408f
AlessioSavi <alessio.savi-cic-it@ibm-com>
2019-06-25 09:50:50 +0200

    Recoded from scratch basic implementation of list

--- Concetti-base/Lista/lista.c
+++ Concetti-base/Lista/lista.c
@@ -1,198 +1,121 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct node
-{
+struct node {
 
   int value;
   struct node *next;
+};
 
-} node_t;
+struct node *init_node(int value);
 
-/* Init the root node of the list*/
-node_t*
-init_node (int value);
+void add_node(struct node **head, int value);
 
-/* Print every element and return the size of the array */
-int
-print_node (node_t *head);
+int remove_node(struct node **head, int value);
 
-/* Link a new node to the last element of the list */
-void
-add_node (node_t **node, int value);
+void print_node(struct node *head);
 
-/* Remove the first occurence of 'value' from the list */
-node_t*
-remove_node (node_t **head, int value);
-
-  void
-display (node_t *head)
-{
-  node_t *it;
-
-  for (it = head; it != NULL; it = it->next)
-    printf (it->next != NULL ? "%d " : "%d\n", it->value);
-}
-
-  node_t*
-init_node (int value)
-{
-  /* Allocate the space for the new node */
-  node_t *node = malloc (sizeof(node_t));
-  /* Use the value in input as data */
-  node->value = value;
-  /* No other element, NULL as next */
-  node->next = NULL;
-  return node;
-}
-
-  int
-print_node (node_t *head)
-{
-  int size = 0;
-  /* Make a copy of the pointer to the first element for iterate the list */
-  node_t *it = head;
-  printf ("\nNode POS [%d] | VALUE [%d] ", size, it->value);
-  while (it->next != NULL)
-  {
-    printf ("\nNode POS [%d] | VALUE [%d] ", ++size, it->value);
-    ++size;
-    it = it->next;
-  }
-  return size;
-}
-
-  void
-add_node (node_t **node, int value)
-{
-
-  if (node == NULL)
-  {
-    printf ("\nadd_node | Call the init_node function before use the list\n");
-    return;
-  }
-
-  /* Root node is alredy populated, so we start from 1 */
-  int counter = 1;
-  /* Iterator for the node */
-  node_t *it = *node;
-
-  // Iterating list ...
-  while (it->next != NULL)
-  {
-    ++counter;
-    it = it->next;
-  }
-
-  printf ("\nInserting element [%d] in position %d", value, counter);
-  it->next = init_node(value);
-  return;
-}
-
-  node_t*
-remove_node (node_t **head, int value)
-{
-  node_t *node = *head;
-  if (node == NULL)
-  {
-    printf ("\nadd_node | Call the init_node function before use the list\n");
+struct node *init_node(int value) {
+  struct node *head = NULL;
+  head = malloc(sizeof(struct node));
+  if (!head) {
+    printf("\nOut of memory!!");
     return NULL;
   }
 
-  int position = 0;
-  /* Check in root node */
-  if (node->value == value)
-  {
-    printf ("\nremove_node | Deleting root node ");
-    /* Copy the element for return */
-    node_t *tmp = init_node (node->value);
-    /* Swap the element with the next one */
-    if (node->next != NULL)
-    {
-      //free (node);
-      *node = *node->next;
-    }
-    else
-    {
-      free (node);
-    }
-    return tmp;
+  head->value = value;
+  head->next = NULL;
+  return head;
+}
+
+void add_node(struct node **head, int value) {
+
+  if (*head == NULL) {
+    *head = init_node(value);
+    printf("\nRoot node initialized [%d]", value);
+    return;
   }
-  /* Iterator for the node */
-  node_t *it = node;
 
-  // Iterating list ...
-  while (it->next != NULL)
-  {
-    ++position;
-    if (it->next->value == value)
-    {
-      printf ("\nremove_node | Removing --> [%d] | POS: [%d]", value,
-          position);
-      node_t *tmp = init_node (it->next->value); /* element to remove */
-      if (it->next->next != NULL)
-      {
-        free (it->next);
-        *it->next = *it->next->next;
-      }
-      else
-      {
-        printf ("\nremove_node | Deleting last element!");
-        // Latest element
-        free (it->next);
-        it->next = NULL;
-      }
-      return tmp;
-    }
-    it = it->next;
+  struct node *temp = *head;
+
+  while (temp->next != NULL) {
+    temp = temp->next;
   }
-  printf ("\nremove_node | Element not found");
-  return NULL;
+  printf("\nAdding node %d", value);
+  temp->next = init_node(value);
+  return;
 }
 
-
-
-void test1(){
-  printf("\n=======================TEST1=====================");
-  node_t *head = init_node(1);
-  add_node(&head,2);
-  add_node(&head,3);
-  add_node(&head,4);
-  add_node(&head,5);
-  add_node(&head,6);
-  print_node(head);
-  free(head);
-  printf("\n=======================TEST1=====================");
+void print_node(struct node *head) {
+  if (head == NULL) {
+    printf("\nprint_node | Call the init function, there is no list there :D");
+  }
+  printf("\n");
+  while (head != NULL) {
+    printf("%d%s", head->value, (head->next != NULL) ? " | " : "");
+    head = head->next;
+  }
+  return;
 }
 
-void test2(){
-  printf("\n=======================TEST2=====================");
-  node_t *head = init_node(1);
-  add_node(&head,2);
-  add_node(&head,3);
-  add_node(&head,4);
-  remove_node(&head,4);
-  print_node(head);
-  free(head);
-  printf("\n=======================TEST2=====================");
+int remove_node(struct node **head, int value) {
+  printf("\nRemove node | Checking if root exist ...");
+  if (*head == NULL) {
+    printf("\nCall the init function, list empty!");
+    return -1;
+  } else if ((*head)->value == value) {
+    printf("\nFound in root! %d", value);
+    *head = (*head)->next;
+    return value;
+  }
 
+  printf("\nRemove node | root exist, iterating list ...");
+  /* Used for iterate the list */
+  struct node *current = (*head)->next;
+  /* Used for swap the next element */
+  struct node *swap_node = NULL;
+
+  printf("\nremove_node | iterating value ...");
+  while (current->next != NULL) {
+    if (current->next->value == value) { /* Data found! */
+      swap_node = current->next;
+      current->next = swap_node->next;
+      free(swap_node);
+
+      // current = (*head)->next; /* try with current->next*/
+      // free(*head);
+      //*head = current;
+
+      printf("\n Data %d found!", value);
+      return value;
+    }
+    current = current->next;
+  }
+  printf("\nData not found - last");
+  return -1;
 }
 
+int main() {
 
-  int
-main ()
-{
-  test1();
-  test2();
-  node_t *head = init_node(1);
-  add_node(&head,2);
-  add_node(&head,3);
-  add_node(&head,4);
-  remove_node(&head,1);
+  struct node *head = NULL;
   print_node(head);
-  free(head);
-
-
-
-
+  add_node(&head, 1);
+  print_node(head);
+  add_node(&head, 2);
+  print_node(head);
+  add_node(&head, 3);
+  print_node(head);
+  add_node(&head, 4);
+  print_node(head);
+  remove_node(&head, 1);
+  print_node(head);
+  remove_node(&head, 2);
+  print_node(head);
+  remove_node(&head, 3);
+  print_node(head);
+  remove_node(&head, 4);
+  print_node(head);
+  add_node(&head, 3);
+  print_node(head);
+  return 0;
 }
